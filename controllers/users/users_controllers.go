3d81c4f9ca3becb1b185830cf6c34d5514a13b75
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2020-11-12 16:38:17 +0200

    First implementation of user controller

--- controllers/users/users_controllers.go
+++ controllers/users/users_controllers.go
@@ -2,11 +2,25 @@ package users
 
 import (
 	"github.com/gofiber/fiber"
+	"github.com/thankala/bookstore_users-api/domain/users"
+	"github.com/thankala/bookstore_users-api/services"
 	"net/http"
 )
 
 func CreateUser(c *fiber.Ctx) error {
-	return c.Status(http.StatusNotImplemented).SendString("Implement")
+	var requestBody users.User
+	err := c.BodyParser(&requestBody)
+	if err != nil {
+		//TODO: Handle Error
+	}
+	result, saveError := services.CreateUser(requestBody)
+	if saveError != nil {
+		//TODO: Handle User Creation
+	}
+	return c.Status(http.StatusCreated).JSON(&fiber.Map{
+		"result": result,
+		"error":  saveError,
+	})
 }
 
 func GetUser(c *fiber.Ctx) error {
