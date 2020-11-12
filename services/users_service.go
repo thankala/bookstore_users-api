2e3e4c3327b1de1ec898c411f809f4f708c1eec8
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2020-11-12 21:21:23 +0200

    Modified service layer

--- services/users_service.go
+++ services/users_service.go
@@ -2,8 +2,9 @@ package services
 
 import (
 	"github.com/thankala/bookstore_users-api/domain/users"
+	"github.com/thankala/bookstore_users-api/utils/errors"
 )
 
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestError) {
 	return &user, nil
 }
