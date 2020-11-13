7fb1d2d24059cb4521da38abaac89baeb1300d68
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2020-11-14 01:12:38 +0200

    Added env variables for mysql connection

--- datasources/mysql/bookstore_users/bookstore_users.go
+++ datasources/mysql/bookstore_users/bookstore_users.go
@@ -1,31 +1,46 @@
 package bookstore_users
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/thankala/bookstore_users-api/domain/users"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
+)
+
+const (
+	mysqlUsersUsername = "MYSQL_USERNAME"
+	mysqlUsersPassword = "MYSQL_PASSWORD"
+	mysqlUsersHost     = "MYSQL_HOST"
+	mysqlUsersPort     = "MYSQL_PORT"
+	mysqlUsersSchema   = "MYSQL_SCHEMA"
 )
 
 var (
-	usersDB *sql.DB
+	Client *gorm.DB
+
+	_        = godotenv.Load("variables.env")
+	username = os.Getenv(mysqlUsersUsername)
+	password = os.Getenv(mysqlUsersPassword)
+	host     = os.Getenv(mysqlUsersHost)
+	port     = os.Getenv(mysqlUsersPort)
+	schema   = os.Getenv(mysqlUsersSchema)
 )
 
 func init() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		"user",
-			"password",
-			"127.0.0.1",
-			"3307",
-			"bookstore_users",
+		username,
+		password,
+		host,
+		port,
+		schema,
 	)
-	usersDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var err error
+	Client, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
 	}
-	log.Println("Database successfully configures")
-
-	usersDB.AutoMigrate(&users.User{})
+	log.Println("Database successfully configured")
 }
